3c6ebbbcc48dfe2c349b2e6f9b6170ecd32badbc
Venessa <v_campbell10@yahoo.com>
2020-05-28 17:54:16 -0400

    working on min jumps

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,10 +2,35 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  const current = linkedlist.head;
+  let slow = current;
+  let fast = current.next;
+  while (slow !== fast && fast !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  if (slow === fast) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+// find where the node value for the beginning of the loop
+// do we call isloop in find loop? the opposite? or another option?
 
+const findLoop = (linkedlist) => {
+  if (isLoop(linkedlist)) {
+    let current = linkedlist.head;
+    let slow = current;
+    let fast = current.next;
+    while (fast.previous === slow) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    return fast.value;
+  }
+};
 /*
 EXTRA CREDIT:
 
@@ -14,4 +39,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = { isLoop, findLoop };
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,15 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
-
+const minJumps = (arr) => {
+  // we need to know length of the array and value
+  // of the index
+  // use the index to find the larger number
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[0] <= arr.length) {
+      //return 1
+    }
+  }
+  return 1;
 };
-
-module.exports = minJumps
+module.exports = minJumps;
