048f1afacf6e40288729a64b3f2aa12daac53d4d
Anna Morozova <annamikmorozova@gmail.com>
2020-05-28 19:42:59 -0400

    Pass Min-jumps. Add new case to resolve next

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -5,11 +5,18 @@ const minJumps = (arr) => {
   // we need to know length of the array and value
   // of the index
   // use the index to find the larger number
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] <= arr.length) {
-      //return 1
+  if (arr[0] >= arr.length - 1) {
+    return 1;
+  }
+  let largest = 0;
+  let index = 0;
+  for (let i = 1; i <= arr[0]; i++) {
+    if (largest < arr[i]) {
+      index = i;
+      largest = arr[i];
     }
   }
-  return 1;
+  return minJumps(arr.slice(index)) + 1
 };
+
 module.exports = minJumps;
