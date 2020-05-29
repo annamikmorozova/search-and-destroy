18bd7cf21565ba0faef44f9cc9eb13f15911bccd
Anna Morozova <annamikmorozova@gmail.com>
2020-05-28 21:59:45 -0400

    Complete min-jumps with extra case

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -11,9 +11,9 @@ const minJumps = (arr) => {
   let largest = 0;
   let index = 0;
   for (let i = 1; i <= arr[0]; i++) {
-    if (largest < arr[i]) {
+    if (largest < arr[i] + i) {
       index = i;
-      largest = arr[i];
+      largest = arr[i] + i;
     }
   }
   return minJumps(arr.slice(index)) + 1
